cd7f86d48aab0b0aea2df2a700ffcbecf163093f
Arfian Adam <arfianadam@yahoo.com>
2017-08-16 17:36:49 +0700

    improved code indentation

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
 class LazyLoadNext {
-	constructor(selector = 'img.lazyload') {
-		this.selector = selector;
-		this.init();
-	}
+  constructor(selector = 'img.lazyload') {
+    this.selector = selector;
+    this.init();
+  }
 
-	init() {
-		let self = this
+  init() {
+    let self = this;
 
-		let els = document.querySelectorAll(this.selector);
+    let els = document.querySelectorAll(this.selector);
 
-		if (!('IntersectionObserver' in window)) {
-      console.log('IntersectionObserver not supported but there is polifill added to the page :)')
+    if (!('IntersectionObserver' in window)) {
+      console.log(
+        'IntersectionObserver not supported but there is polifill added to the page :)'
+      );
 
       let polyfill = document.createElement('script');
-      polyfill.src = 'https://rawgit.com/jeremenichelli/intersection-observer-polyfill/master/intersection-observer-polyfill.js';
+      polyfill.src =
+        'https://rawgit.com/jeremenichelli/intersection-observer-polyfill/master/intersection-observer-polyfill.js';
       document.head.appendChild(polyfill);
 
       polyfill.onload = () => {
-      	self.createObserver();
-      }
+        self.createObserver();
+      };
     } else {
-    	self.createObserver();
+      self.createObserver();
     }
-	}
+  }
 
-	createObserver() {
-		const elements = document.querySelectorAll(this.selector);
-		const observer = new IntersectionObserver((entries, observer) => {
+  createObserver() {
+    const elements = document.querySelectorAll(this.selector);
+    const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
         if (entry.isIntersecting && entry.target.getAttribute('data-src')) {
           entry.target.src = entry.target.getAttribute('data-src');
           entry.target.removeAttribute('data-src');
         }
-      })
-		}, {});
-		Array.prototype.map.call(elements, function(item) {
-      observer.observe(item)
-    })
-	}
+      });
+    }, {});
+    Array.prototype.map.call(elements, function(item) {
+      observer.observe(item);
+    });
+  }
 }
 
 if (typeof module == 'undefined') {
-	module.exports = LazyLoadNext;
+  module.exports = LazyLoadNext;
 } else {
-	window.LazyLoadNext = LazyLoadNext;
+  window.LazyLoadNext = LazyLoadNext;
 }
