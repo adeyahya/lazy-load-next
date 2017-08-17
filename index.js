1f7fb1cd035ebd51d7004aa88877d53192a640c0
Ade Yahya <adeyahyaprasetyo@gmail.com>
2017-08-18 02:51:16 +0700

    remove class lazyload when loaded

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ class LazyLoadNext {
         if (entry.isIntersecting && entry.target.getAttribute('data-src')) {
           entry.target.src = entry.target.getAttribute('data-src');
           entry.target.removeAttribute('data-src');
-          entry.target.classList.remove(this.selector); // remove lazyload class
+          entry.target.classList.remove('lazyload'); // remove lazyload class
           observer.unobserve(entry.target); // remove the listener
         }
       });
